648d3a5d4a82cd427e6cfcce5ba216c7dd0f5903
TimDM17 <TimDucMinh@gmx.de>
2025-06-24 10:57:03 +0200

    Loading grammar and ruleset paths

--- nsfr/common.py
+++ nsfr/common.py
@@ -5,4 +5,8 @@ from nsfr.utils.logic import get_lang, build_infer_module
 from nsfr.nsfr import NSFReasoner
 from nsfr.valuation import ValuationModule
 
-
+def get_nsfr_model(env_name: str, rules: str, device: str, train=False, explaine=False):
+    
+    current_path = os.path.dirname(__file__)
+    lark_path = os.path.join(current_path, 'lark/exp.lark')
+    lang_base_path = f"in/envs/{env_name}/logic/"
